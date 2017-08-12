62defe6aebaadd39c7a7871848477d7cffe1e8b6
Joe Crockett <jcrocket@gmail.com>
2017-08-12 17:29:36 -0400

    updates to style

---
+++ src/webparts/recruitmentDashboard/components/IStep.ts
@@ -0,0 +1,4 @@
+export interface IStep {
+    lane: number;
+    bgColor: string;
+}
--- src/webparts/recruitmentDashboard/components/RecruitmentApi.tsx
+++ src/webparts/recruitmentDashboard/components/RecruitmentApi.tsx
@@ -4,8 +4,10 @@ import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 import { IRecruitmentApiProps } from './IRecruitmentApiProps';
 import { IRecruitmentApiState } from './IRecruitmentApiState';
 import { IListItem } from './IListItem';
+import { IStep } from './IStep';
 export class RecruitmentApi extends React.Component<IRecruitmentApiProps, IRecruitmentApiState> {
     private listItemEntityTypeName: string = undefined;
+
     constructor(props: IRecruitmentApiProps, state: IRecruitmentApiState) {
         super(props);
 
@@ -15,6 +17,7 @@ export class RecruitmentApi extends React.Component<IRecruitmentApiProps, IRecru
         };
 
         this.handleChange = this.handleChange.bind(this);
+
     }
 
     public componentWillReceiveProps(nextProps: IRecruitmentApiProps): void {
@@ -32,18 +35,28 @@ export class RecruitmentApi extends React.Component<IRecruitmentApiProps, IRecru
         })
     }
 
+    componentDidMount() {
+        let load: boolean = this.listNotConfigured(this.props);
+        if (!load) {
+            this.readItems();
+        }
+    }
+
     public render(): React.ReactElement<IRecruitmentApiProps> {
+
         const items: JSX.Element[] = this.state.items.map((item: IListItem, i: number): JSX.Element => {
             let stepDesc: string = item.vrStep;
             let step: string = stepDesc.slice(1, 3);
-            let lane: number = this.figureOutLaneFromStep(step);
-            let bgColor: string = this.figureOutAssignmentFromStep(step);
+            let lane: number = (this.figureOutLaneFromStep(step)).lane;
+            let lanePush: number = lane - 1;
+            let bgColor: string = (this.figureOutLaneFromStep(step)).bgColor;
             return (
                 <li className={`${styles.li}`}>
                     <div className="ms-Grid-row">
-                        <div className={`ms-bgColor-${bgColor} ms-fontColor-white ms-Grid-col ms-u-lg1 ms-u-lgPush${lane}`}>
-                            {item.Title} {lane}
+                        <div className={`ms-bgColor-${bgColor} ms-fontColor-white ms-Grid-col ms-u-lg1 ms-u-lgPush${lanePush}`} >
+                            {item.Title}
                         </div>
+
                     </div>
                 </li>
             );
@@ -56,28 +69,100 @@ export class RecruitmentApi extends React.Component<IRecruitmentApiProps, IRecru
             <div>
                 {this.state.status}
                 <div >
-                    <a href="#" className={`${styles.button} ${disabled}`} onClick={() => this.readItems()}>
-                        <span className={styles.label}>Read all items</span>
-                    </a>
-                </div>
-                <div className={`ms-Grid-row  ${styles.row}`}>
-                    <div className="ms-Grid-col ms-u-lg1">1</div>
-                    <div className="ms-Grid-col ms-u-lg2">2</div>
-                    <div className="ms-Grid-col ms-u-lg3">3</div>
-                    <div className="ms-Grid-col ms-u-lg4">4</div>
-                    <div className="ms-Grid-col ms-u-lg5">5</div>
-                    <div className="ms-Grid-col ms-u-lg6">6</div>
-                    <div className="ms-Grid-col ms-u-lg7">7</div>
-                    <div className="ms-Grid-col ms-u-lg8">8</div>
-                    <div className="ms-Grid-col ms-u-lg9">9</div>
-                    <div className="ms-Grid-col ms-u-lg10">10</div>
-                    <div className="ms-Grid-col ms-u-lg11">11</div>
-                    <div className="ms-Grid-col ms-u-lg12">12</div>
+                    <ul className={`${styles.ul}`}>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">1</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">2</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">3</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">4</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">5</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">6</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">7</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">8</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">9</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">10</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">11</div>
+                            </div>
+                        </li>
+                        <li className={`${styles.li}`}>
+                            <div className={`${styles.fakerow}`}>
+                                <div className="ms-Grid-col ms-u-lg1">12</div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
                 <div >
                     <ul className={`${styles.ul}`}>
                         {items}
                     </ul>
+                </div>
+                <div className={`${styles.key}`}>
+                    <div className="ms-Grid-row">
+                        <div className="ms-Grid-col ms-u-lg3">
+                            <p>Color Key</p>
+                        </div>
+                    </div>
+                    <div className="ms-Grid-row">
+                        <div className="ms-bgColor-themeDark ms-fontColor-white ms-Grid-col ms-u-lg3">
+                            <p>HR Liaison</p>
+                        </div>
+                    </div>
+                    <div className="ms-Grid-row">
+                        <div className="ms-bgColor-green ms-fontColor-white ms-Grid-col ms-u-lg3">
+                            <p>Human Resources</p>
+                        </div>
+                    </div>
+                    <div className="ms-Grid-row">
+                        <div className="ms-bgColor-yellowLight ms-Grid-col ms-u-lg3">
+                            <p>OMB</p>
+                        </div>
+                    </div>
+                    <div className="ms-Grid-row">
+                        <div className="ms-bgColor-blueMid ms-fontColor-white ms-Grid-col ms-u-lg3">
+                            <p>NeoGov</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
@@ -100,7 +185,7 @@ export class RecruitmentApi extends React.Component<IRecruitmentApiProps, IRecru
             })
             .then((response: { value: IListItem[] }): void => {
                 this.setState({
-                    status: `Successfully loaded ${response.value.length} items`,
+                    status: `${response.value.length} vacancies in the pipeline.`,
                     items: response.value
                 });
             }, (error: any): void => {
@@ -116,19 +201,22 @@ export class RecruitmentApi extends React.Component<IRecruitmentApiProps, IRecru
             props.listName.length === 0;
     }
 
-    private figureOutLaneFromStep(stepStr: string): number {
+    private figureOutLaneFromStep(stepStr: string): IStep {
         let lane: number;
+        let bgColor: string = "themeDark";
         let stepNum: number = Number(stepStr);
         if (stepNum <= 2) {
             lane = 1;
         } else if (stepNum === 3) {
             lane = 2;
+            bgColor = "yellowLight";
         } else if (stepNum <= 7) {
             lane = 3;
         } else if (stepNum === 8) {
             lane = 4;
         } else if (stepNum === 9) {
             lane = 4;
+            bgColor = "blueMid";
         } else if (stepNum <= 11) {
             lane = 5;
         } else if (stepNum === 12) {
@@ -141,51 +229,31 @@ export class RecruitmentApi extends React.Component<IRecruitmentApiProps, IRecru
             lane = 9;
         } else if (stepNum === 26) {
             lane = 9;
+            bgColor = "green";
         } else if (stepNum <= 30) {
             lane = 10;
         } else if (stepNum <= 36) {
             lane = 11;
         } else if (stepNum === 37) {
             lane = 12;
+            bgColor = "blueMid";
         } else {
             lane = 1;
+            bgColor = "themeDark";
         }
-        return lane;
+        let stepOutput: IStep = {
+            lane: lane,
+            bgColor: bgColor
+        }
+        return stepOutput;
     }
-    private figureOutAssignmentFromStep(stepStr: string): string {
-        let bgColor: string;
-        let stepNum: number = Number(stepStr);
-        if (stepNum <= 2) {
-            bgColor = "themeDark";
-        } else if (stepNum === 3) {
-            bgColor = "yellowLight";
-        } else if (stepNum <= 7) {
-            bgColor = "themeDark";
-        } else if (stepNum === 8) {
-            bgColor = "themeDark";
-        } else if (stepNum === 9) {
-            bgColor = "blueMid";
-        } else if (stepNum <= 11) {
-            bgColor = "green";
-        } else if (stepNum === 12) {
-            bgColor = "themeDark";
-        } else if (stepNum <= 17) {
-            bgColor = "themeDark";
-        } else if (stepNum <= 19) {
-            bgColor = "themeDark";
-        } else if (stepNum <= 25) {
-            bgColor = "themeDark";
-        } else if (stepNum === 26) {
-            bgColor = "green";
-        } else if (stepNum <= 30) {
-            bgColor = "themeDark";
-        } else if (stepNum <= 36) {
-            bgColor = "themeDark";
-        } else if (stepNum === 37) {
-            bgColor = "blueMid";
+
+    private showDetails() {
+        var x = document.getElementById('myDIV');
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
         } else {
-            bgColor = "themeDark";
+            x.style.display = 'none';
         }
-        return bgColor;
     }
 }
